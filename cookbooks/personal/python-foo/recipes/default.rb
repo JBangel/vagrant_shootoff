8bb6a9b5ea00c5e4666e3dc841bcb8cb463ad666
Joshua Bangel <falornan@gmail.com>
2013-05-13 01:00:46 -0500

    Added upstart script for python-foo

--- cookbooks/personal/python-foo/recipes/default.rb
+++ cookbooks/personal/python-foo/recipes/default.rb
@@ -13,3 +13,16 @@ execute "Install python_foo requirements" do
     cwd     "/vagrant/python_foo"
     command "/vagrant/python_foo/bin/pip install -r Requirements.txt"
 end
+
+template "/etc/init/flask.conf" do
+  group   "root"
+  owner   "root"
+  source  "upstart.erb"
+  variables(:venv_path => "/vagrant/python_foo")
+end
+
+service "flask" do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
