782f1610e17b8fbdb05c2fb700ce6762133d85ae
Joshua Bangel <falornan@gmail.com>
2013-05-12 21:32:43 -0500

    Added the vagrant-shootoff driving cookbook

---
+++ cookbooks/personal/vagrant-shootoff/metadata.rb
@@ -0,0 +1,11 @@
+name              "shootoff"
+maintainer        "Joshua Bangel"
+maintainer_email  "falornan@gmail.com"
+description       "Installs/configures the required components for the vagrant shootoff"
+version           "1.0.0"
+
+depends           "vim"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
